1e099992b859a36a0cd77b575890e2c7082cdadf
Tedi Yankov <149787177+tediyankov@users.noreply.github.com>
2024-05-27 22:59:20 +0100

    Create scale.fix.js

---
+++ _layouts/scale.fix.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var details = document.querySelectorAll("details");
+    details.forEach(function(detail) {
+      detail.addEventListener("toggle", function() {
+        // Optional: You can add functionality here if needed
+      });
+    });
+  });
+</script>
